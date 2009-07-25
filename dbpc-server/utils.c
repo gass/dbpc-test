fa5d7d92a686deac7019c4df4fec3e010e973b84
Luis Matos <gass@otiliamatos.ath.cx>
2009-07-25 21:34:33 +0100

    Added the utils file with strdup_printf function

---
+++ dbpc-server/utils.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+char *
+strdup_printf (const char *fmt, ...)
+{
+  /* Guess we need no more than 100 bytes. */
+  int n, size = 100;
+  char *p;
+  va_list ap;
+
+  if ((p = malloc (size)) == NULL)
+    return NULL;
+  while (1)
+    {
+      /* Try to print in the allocated space. */
+      va_start (ap, fmt);
+#ifdef WIN32
+      n = _vsnprintf (p, size, fmt, ap);
+#else
+      n = vsnprintf (p, size, fmt, ap);
+#endif
+      va_end (ap);
+      /* If that worked, return the string. */
+      if (n > -1 && n < size)
+        return p;
+      /* Else try again with more space. */
+      if (n > -1)               /* glibc 2.1 */
+        size = n + 1;           /* precisely what is needed */
+      else                      /* glibc 2.0 */
+        size *= 2;              /* twice the old size */
+      if ((p = realloc (p, size)) == NULL)
+        return NULL;
+    }
+}
---
+++ dbpc-server/utils.h
@@ -0,0 +1,10 @@
+#ifndef UTILS_H
+#define UTILS_H
+#include <stdio.h>
+#include <stdarg.h>
+
+char *
+strdup_printf (const char *fmt, ...)
+
+
+#endif /* UTILS_H */
