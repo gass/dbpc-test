862aeb3e52a6fa738d3dbc7dc7789fa92d9d20da
Luis Matos <gass@otiliamatos.ath.cx>
2009-07-26 14:36:22 +0100

    bug in tag list search.

--- dbpc-server/tag_list.c
+++ dbpc-server/tag_list.c
@@ -11,6 +11,7 @@ void dbpc_tag_list_add(DBPCTag * t)
 {
 	if (TAG_LIST == NULL) {
 		TAG_LIST = t;
+        t->next = NULL;
 		return;
 	} else {
 		dbpc_tag_list_add_aux(TAG_LIST, t);
@@ -21,6 +22,7 @@ static void dbpc_tag_list_add_aux(DBPCTag * tag, DBPCTag * t)
 {
 	if (tag->next == NULL) {
 		tag->next = t;
+        t->next = NULL;
 	} else {
 		dbpc_tag_list_add_aux(tag->next, t);
 	}
@@ -36,10 +38,10 @@ static DBPCTag *dbpc_tag_list_find_aux (DBPCTag *tag, const char *tag_name)
     if (tag == NULL) {
         return NULL;
     }
-    if (!strcmp (tag->name, tag_name)) {
+    if ((strcmp(tag->name, tag_name) == 0)) {
         return tag;
     } else {
-        return dbpc_tag_list_find_aux (tag, tag_name);
+        return dbpc_tag_list_find_aux (tag->next, tag_name);
     }
     
 }
