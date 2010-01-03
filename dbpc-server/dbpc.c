75ff060924c269ab9b6d3796173f65bcded33320
Luis Matos <gass@otiliamatos.ath.cx>
2010-01-03 20:40:47 +0000

    Updated the messy example file dbpc.c

--- dbpc-server/dbpc.c
+++ dbpc-server/dbpc.c
@@ -54,23 +54,42 @@ void create_source(void)
 	
 	printf ("eeer\n");
 	printf ("descrition: %s\n", src->description);
-	cn = dbpc_connection_new(src, "/home/gass/Desktop/tt.txt");
+	cn = dbpc_connection_new(src, "/home/gass/Desktop/tt2.dbpc");
 	/* bool tag */
-	btag = dbpc_bool_new(cn, "Luis Luis", "bool");
-	dbpc_tag_set_permission (DBPC_TAG(btag), R);
+	btag = dbpc_bool_new(cn, "Luis Luis", "M6.1");
+	dbpc_tag_set_permission (DBPC_TAG(btag), RW);
+	dbpc_tag_set_update_mode (DBPC_TAG(btag), CONTINUOUS);
+	dbpc_tag_set_operation (DBPC_TAG(btag), R);
 	dbpc_tag_list_add(DBPC_TAG(btag));
-	dbpc_bool_dump (btag);
+	//dbpc_bool_dump (btag);
 	printf ("COUNT %d\n", dbpc_tag_list_count ());
 	
 	/* bool int */
-	itag = dbpc_int_new(cn, "Luis Luis2", "int");
-	dbpc_int_dump (itag);
+	itag = dbpc_int_new(cn, "Luis Luis2", "M0");
 	dbpc_tag_set_update_mode (DBPC_TAG(itag), CONTINUOUS);
 	dbpc_tag_set_permission (DBPC_TAG(itag), RW);
-	dbpc_int_write (itag, 1000);
+	dbpc_tag_set_operation (DBPC_TAG(itag), R);
 	dbpc_tag_list_add(DBPC_TAG(itag));
-	dbpc_int_dump (itag);
-	itag = DBPC_TAG_INT(dbpc_tag_list_find("Luis Luis2"));
+	itag = dbpc_int_new(cn, "Luis Luis2", "M1");
+	dbpc_tag_set_update_mode (DBPC_TAG(itag), CONTINUOUS);
+	dbpc_tag_set_permission (DBPC_TAG(itag), RW);
+	dbpc_tag_set_operation (DBPC_TAG(itag), R);
+	dbpc_tag_list_add(DBPC_TAG(itag));
+	itag = dbpc_int_new(cn, "Luis Luis2", "M2");
+	dbpc_tag_set_update_mode (DBPC_TAG(itag), CONTINUOUS);
+	dbpc_tag_set_permission (DBPC_TAG(itag), RW);
+	dbpc_tag_set_operation (DBPC_TAG(itag), R);
+	dbpc_tag_list_add(DBPC_TAG(itag));
+	itag = dbpc_int_new(cn, "Luis Luis2", "M3");
+	dbpc_tag_set_update_mode (DBPC_TAG(itag), CONTINUOUS);
+	dbpc_tag_set_permission (DBPC_TAG(itag), RW);
+	dbpc_tag_set_operation (DBPC_TAG(itag), R);
+	dbpc_tag_list_add(DBPC_TAG(itag));
+//	dbpc_int_write (itag, 1000);
+	//dbpc_tag_list_add(DBPC_TAG(itag));
+//	dbpc_int_dump (itag);
+	//itag = DBPC_TAG_INT(dbpc_tag_list_find("Luis Luis2"));
+	/*
 	dbpc_int_write (itag, 1001);
 	dbpc_int_dump (itag);
 	
@@ -78,15 +97,20 @@ void create_source(void)
 	dbpc_bool_dump (btag);
 	dbpc_int_read (itag);
 	dbpc_int_dump (itag);
-	
+	 */
+	printf ("COUNT %d\n", dbpc_tag_list_count ());
 		dbpc_connection_start (cn);
-		dbpc_tag_process (DBPC_TAG(itag));
-		dbpc_tag_process (DBPC_TAG(itag));
-		dbpc_int_dump (itag);
-		dbpc_tag_process (DBPC_TAG(btag));
-		dbpc_bool_dump (btag);
+		//dbpc_tag_process (DBPC_TAG(itag));
+		//dbpc_tag_process (DBPC_TAG(itag));
+		//dbpc_int_dump (itag);
+		dbpc_list_process ();
+		//dbpc_bool_dump (btag);
+		//dbpc_list_execute(DBPC_LIST_EXECUTE(dbpc_int_dump))
 		dbpc_connection_stop (cn);
-	
+		dbpc_bool_dump (DBPC_TAG(btag));
+		
+		
+/*	
 	printf ("COUNT %d\n", dbpc_tag_list_count ());
 	dbpc_tag_list_remove (DBPC_TAG(btag));
 	printf ("COUNT %d\n", dbpc_tag_list_count ());
@@ -94,5 +118,5 @@ void create_source(void)
 	dbpc_tag_list_remove (DBPC_TAG(itag));
 	printf ("COUNT %d\n", dbpc_tag_list_count ());
 	dbpc_int_free(itag);
-	
+*/	
 }
